bc58a65349fde39db18705ccff0a3df02d66c1c5
Tom Almeida <tom@tommoa.me>
2021-01-26 09:14:20 +0800

    nixpkgs: Add darwin configuration to repository
    Nix-darwin[0] is a way of managing a system configuration of a
    darwin-based system through Nix. This allows me to declare the packages
    that I want to use and have them automatically updated and work across
    package version.
    
    The trickiest thing about this so far has been the method with which
    launchd agents (specifically those for goimapnotify) are handled.
    
    [0] https://github.com/LnL7/nix-darwin

---
+++ .config/nixpkgs/darwin/configuration.nix
@@ -0,0 +1,88 @@
+{ config, lib, pkgs, ... }:
+
+{
+  nixpkgs.overlays = [
+    (import ../overlays)
+  ];
+  nixpkgs.config.allowUnsupportedSystem = true;
+  environment.systemPackages = [
+    pkgs.aerc
+    pkgs.alacritty
+    pkgs.bat
+    pkgs.beancount
+    pkgs.exa
+    pkgs.fd
+    pkgs.git
+    pkgs.goimapnotify
+    pkgs.gnupg
+    pkgs.isync
+    pkgs.jq
+    pkgs.khal
+    pkgs.khard
+    pkgs.less
+    pkgs.msmtp
+    pkgs.neovim
+    pkgs.nodejs
+    pkgs.notmuch
+    pkgs.ripgrep
+    pkgs.tmux
+    pkgs.vdirsyncer
+  ];
+
+  system.keyboard = {
+    enableKeyMapping = true;
+    remapCapsLockToEscape = true;
+  };
+
+  system.defaults.dock = {
+    autohide = true;
+    showhidden = true;
+  };
+
+  services.yabai = {
+    enable = true;
+    package = pkgs.yabai;
+    # enableScriptingAddition = true;
+  };
+
+  services.skhd = {
+    enable = true;
+    package = pkgs.skhd;
+  };
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  # services.nix-daemon.enable = true;
+  nix.package = pkgs.nix;
+
+  launchd.user.agents = let goimapnotifyconfs = (builtins.filter (filename: (builtins.match ".*\.conf" filename) != null) (builtins.attrNames (builtins.readDir ~/.config/imapnotify)));
+  in
+  builtins.listToAttrs
+    (builtins.map
+      (name: {
+        name = "goimapnotify${name}";
+        value = {
+          serviceConfig = {
+            ProgramArguments = [
+              "${pkgs.goimapnotify}/bin/goimapnotify"
+              "-conf"
+              (toString ~/.config/imapnotify + "/${name}")
+            ];
+            RunAtLoad = true;
+            EnvironmentVariables = {
+              PATH = "${pkgs.goimapnotify}/bin:${config.environment.systemPath}";
+            };
+            KeepAlive = true;
+          };
+        };
+      }) goimapnotifyconfs);
+
+  programs.zsh.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+}

87255277306a5cb22d581eba55ccf4e45cb1a5c5
Tom Almeida <tom@tommoa.me>
2021-01-26 09:40:17 +0800

    nixpkgs: Add overlay for yabai
    My work macbook is on macOS 11.1.0, which is incompatible with yabai
    3.3.4 in nixpkgs. This overlay changes the yabai version to 3.3.6, which
    has support for 11.1.0.

---
+++ .config/nixpkgs/overlays/default.nix
@@ -0,0 +1,19 @@
+self: super:
+
+{
+  yabai = super.yabai.overrideAttrs (o: rec {
+    version = "3.3.6";
+    src = builtins.fetchTarball {
+      url = "https://github.com/koekeishiya/yabai/releases/download/v${version}/yabai-v${version}.tar.gz";
+      sha256 = "00iblhdx89wyvasl3hm95w91z4mrwb7pbfdvg9cmpcnqphbfs5ld";
+    };
+
+    
+    installPhase = ''
+      mkdir -p $out/bin
+      mkdir -p $out/share/man/man1/
+      cp ./archive/bin/yabai $out/bin/yabai
+      cp ./archive/doc/yabai.1 $out/share/man/man1/yabai.1
+    '';
+  });
+}

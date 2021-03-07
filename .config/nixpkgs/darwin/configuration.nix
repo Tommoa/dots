f9a6f337bc8cd027325f7a0f82b598478314e59f
Tom Almeida <tom@tommoa.me>
2021-03-07 14:56:59 +0800

    nixpkgs(darwin): Manage build users
    ngl, I don't really understand this option, but nix yelled at me until I
    added it, so...

--- .config/nixpkgs/darwin/configuration.nix
+++ .config/nixpkgs/darwin/configuration.nix
@@ -116,6 +116,8 @@
 
   programs.zsh.enable = true;
 
+  users.nix.configureBuildUsers = true;
+
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;

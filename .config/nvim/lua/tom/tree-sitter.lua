376ec9847681fc6f023d62619cfea21e1fcc07d9
Tom Almeida <tom@tommoa.me>
2021-07-14 23:39:15 +0800

    nvim: Disable treesitter for i686
    It looks like treesitter doesn't have 32-bit binaries, and seems to not
    let me compile them. I'll likely be able to remove this check eventually
    after everything I use moves to x64.

--- .config/nvim/lua/tom/tree-sitter.lua
+++ .config/nvim/lua/tom/tree-sitter.lua
@@ -1,10 +1,12 @@
 local configs = require('nvim-treesitter.configs')
+local uname = vim.loop.os_uname()
 
 configs.setup {
     ensure_installed = "maintained",
     ignore_install = {},
     highlight = {
-        enable = true,
+        -- Treesitter doesn't have 32-bit binaries
+        enable = uname.machine ~= "i686",
         disable = {},
     },
 }

a5ae4bdd045b5020068e04815fd18dd1df371435
Tom Almeida <toma@arista.com>
2022-02-21 21:26:36 -0800

    feat(guix): Make sure my personal channel works across usernames
    I have a different account name for my work account, so running guix
    with the work account results in the channel being at the wrong path.
    This can be fixed by making it depend on the value of `$HOME`

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -8,7 +8,7 @@
             "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
        (channel
          (name 'tommoa)
-         (url "file:///home/tommoa/docs/guix-overrides"))
+         (url (string-append "file://" (getenv "HOME") "/docs/guix-overrides")))
        ;; This should be uncommented when not developing for tommoa
        ;; (channel
        ;;   (name 'tommoa)

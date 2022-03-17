0db96bd4caa3759a3d62c4a40bf619da1db74422
Tom Almeida <tom@tommoa.me>
2022-03-17 11:45:42 +0800

    chore(nixpkgs): Update yabai to 4.0.0

--- .config/nixpkgs/overlays/default.nix
+++ .config/nixpkgs/overlays/default.nix
@@ -2,18 +2,18 @@ self: super:
 
 {
   yabai = super.yabai.overrideAttrs (o: rec {
-    version = "3.3.10";
+    version = "4.0.0";
     src = builtins.fetchTarball {
       url = "https://github.com/koekeishiya/yabai/releases/download/v${version}/yabai-v${version}.tar.gz";
-      sha256 = "025ww9kjpy72in3mbn23pwzf3fvw0r11ijn1h5pjqvsdlak91h9i";
+      sha256 = "1iwzan3mgayfkx7qbbij53hkxvr419b6kmypp7zmvph270yzy4r9";
     };
 
     
     installPhase = ''
       mkdir -p $out/bin
       mkdir -p $out/share/man/man1/
-      cp ./archive/bin/yabai $out/bin/yabai
-      cp ./archive/doc/yabai.1 $out/share/man/man1/yabai.1
+      cp ./bin/yabai $out/bin/yabai
+      cp ./doc/yabai.1 $out/share/man/man1/yabai.1
     '';
   });
 

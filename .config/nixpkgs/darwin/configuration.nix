23e891826c1b2f63668dc66867fba7fa43fde0d1
Tom Almeida <tom@tommoa.me>
2022-08-29 09:32:32 +0800

    fix(nixpkgs): Use updated configureBuildUsers variable
    This got moved recently.

--- .config/nixpkgs/darwin/configuration.nix
+++ .config/nixpkgs/darwin/configuration.nix
@@ -129,7 +129,7 @@
 
   programs.zsh.enable = true;
 
-  users.nix.configureBuildUsers = true;
+  nix.configureBuildUsers = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog

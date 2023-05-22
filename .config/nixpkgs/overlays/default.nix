365b7e8595be663ddb0d097541c89086a4372f33
Tom Almeida <tom@tommoa.me>
2023-05-22 17:00:24 +0200

    feat(nixpkgs): Upgrade yabai to v5.0.5

--- .config/nixpkgs/overlays/default.nix
+++ .config/nixpkgs/overlays/default.nix
@@ -2,10 +2,10 @@ self: super:
 
 {
   yabai = super.yabai.overrideAttrs (o: rec {
-    version = "5.0.4";
+    version = "5.0.5";
     src = builtins.fetchTarball {
       url = "https://github.com/koekeishiya/yabai/releases/download/v${version}/yabai-v${version}.tar.gz";
-      sha256 = "08cxg94885p7vr8102wk6mad95cnfx31xd7zybxynjwd9x0958pr";
+      sha256 = "0yll7f708ibx3r33bj2jyin8yr39jj7fgfxfsmj8i22857k0w3py";
     };
 
     postPatch = '''';

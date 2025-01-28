e8358bf7d31b64e0b41fe945179347f7f34a64f7
Tom Almeida <tom@tommoa.me>
2025-01-28 12:23:07 +1100

    feat(nixpkgs): Move yabai configuration over to within nix configuration
    This mostly works, although I've noticed that sometimes the scripting
    addition doesn't get enabled correctly.

--- .config/nixpkgs/darwin/configuration.nix
+++ .config/nixpkgs/darwin/configuration.nix
@@ -61,7 +61,45 @@
   services.yabai = {
     enable = true;
     package = pkgs.yabai;
-    # enableScriptingAddition = true;
+    enableScriptingAddition = true;
+
+    config = {
+      # Global settings
+      mouse_follows_focus        = "on";
+      focus_follows_mouse        = "autoraise";
+      window_placement           = "second_child";
+      window_topmost             = "off";
+      window_shadow              = "on";
+      window_opacity             = "off";
+      window_opacity_duration    = 0.0;
+      active_window_opacity      = 1.0;
+      normal_window_opacity      = 0.90;
+      window_border              = "off";
+      window_border_width        = 2;
+      active_window_border_color = "0xff775759";
+      normal_window_border_color = "0xff555555";
+      insert_feedback_color      = "0xffd75f5f";
+      split_ratio                = 0.50;
+      auto_balance               = "off";
+      mouse_modifier             = "fn";
+      mouse_action1              = "move";
+      mouse_action2              = "resize";
+      mouse_drop_action          = "swap";
+
+      # General space
+      layout                     = "bsp";
+      top_padding                = 12;
+      bottom_padding             = 12;
+      left_padding               = 12;
+      right_padding              = 12;
+      window_gap                 = 06;
+    };
+
+    extraConfig = ''
+      yabai -m rule --add app='System Settings' manage=off
+      yabai -m rule --add app='Obsidian' space=5
+      yabai -m signal --add event=dock_did_restart action="sudo yabai --load-sa"
+    '';
   };
 
   services.skhd = {

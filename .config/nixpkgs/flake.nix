d64901fd51729c51f3579caf1ae241bfaf5b53c4
Tom Almeida <tom@tommoa.me>
2025-06-29 11:22:19 +1000

    feat: Add NixOS configuration for 'james' machine with Home Manager
    This adds the NixOS configuration for my desktop computer.

--- .config/nixpkgs/flake.nix
+++ .config/nixpkgs/flake.nix
@@ -3,17 +3,33 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
-
     nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-25.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
   };
 
-  outputs = inputs@{ nix-darwin, ... }: {
+  outputs = inputs@{ nixpkgs, nix-darwin, home-manager, ... }: {
     darwinConfigurations."apollo" = nix-darwin.lib.darwinSystem {
       system = "aarch64-darwin";
       modules = [ ./darwin/configuration.nix ];
+      specialArgs = { inherit inputs; };
+    };
+
+    nixosConfigurations."james" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./nixos/configuration.nix
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.tommoa = import ./home.nix;
+        }
+      ];
       specialArgs = { inherit inputs; };
     };
   };
---
+++ .config/nixpkgs/home.nix
@@ -0,0 +1,70 @@
+{ config, pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    # Standard terminal tools.
+    bat
+    eza
+    fd
+    git
+    gnupg
+    gnumake
+    jq
+    ripgrep
+    tmux
+
+    # language support
+    clang
+    nodejs
+    python3
+    rustup
+    typescript
+    uv
+    # language servers
+    pyright
+    nixd
+    sumneko-lua-language-server
+    typescript-language-server
+    # AI servers/helpers
+    codex
+    ollama
+    opencode
+
+    # terminal + editing
+    alacritty
+    neovim
+
+    # applications
+    discord
+    obsidian
+  ];
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "25.05";
+
+  gtk = {
+    enable = true;
+    iconTheme = {
+      name = "Pop-dark";
+      package = pkgs.pop-icon-theme;
+    };
+    cursorTheme = {
+      name = "Pop";
+      package = pkgs.pop-gtk-theme;
+    };
+    theme = {
+      name = "Pop-dark";
+      package = pkgs.pop-gtk-theme;
+    };
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
